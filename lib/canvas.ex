8385d869820bb0f41b00c780e1dd4b2614790cfd
Chris Wilding <hello@chriswilding.co.uk>
2019-07-23 20:10:05 +0100

    Implement canvas methods

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -1,18 +1,154 @@
 defmodule Canvas do
-  @moduledoc """
-  Documentation for Canvas.
-  """
+  @colour_to_ansi_fn %{
+    black: &IO.ANSI.black_background/0,
+    blue: &IO.ANSI.blue_background/0,
+    cyan: &IO.ANSI.cyan_background/0,
+    green: &IO.ANSI.green_background/0,
+    light_black: &IO.ANSI.light_black_background/0,
+    light_blue: &IO.ANSI.light_blue_background/0,
+    light_cyan: &IO.ANSI.light_cyan_background/0,
+    light_green: &IO.ANSI.light_green_background/0,
+    light_magenta: &IO.ANSI.light_magenta_background/0,
+    light_red: &IO.ANSI.light_red_background/0,
+    light_white: &IO.ANSI.light_white_background/0,
+    light_yellow: &IO.ANSI.light_yellow_background/0,
+    magenta: &IO.ANSI._magenta_background/0,
+    red: &IO.ANSI.red_background/0,
+    white: &IO.ANSI.white_background/0,
+    yellow: &IO.ANSI.yellow_background/0
+  }
+  @allowed_colours Map.keys(@colour_to_ansi_fn)
+  @default_colour :red
 
-  @doc """
-  Hello world.
+  defstruct cells: %{}, row_count: 0, column_count: 0
 
-  ## Examples
+  defguardp is_in_canvas(row_count, column_count, row, column)
+            when row > 0 and row <= row_count and column > 0 and
+                   column <= column_count
 
-      iex> Canvas.hello()
-      :world
+  defguardp is_allowed_colour(colour) when colour in @allowed_colours
 
-  """
-  def hello do
-    :world
+  def new(row_count, column_count) do
+    %Canvas{row_count: row_count, column_count: column_count}
+  end
+
+  def draw_in_cell(
+        %Canvas{cells: cells, row_count: row_count, column_count: column_count} = canvas,
+        row,
+        column,
+        colour
+      )
+      when is_in_canvas(row_count, column_count, row, column) and
+             is_allowed_colour(colour) do
+    new_cells = Map.put(cells, {row, column}, colour)
+    %{canvas | cells: new_cells}
+  end
+
+  def draw_in_row(
+        %Canvas{row_count: row_count, column_count: column_count} = canvas,
+        row,
+        from_column,
+        to_column,
+        colour
+      )
+      when is_in_canvas(row_count, column_count, row, from_column) and
+             is_in_canvas(row_count, column_count, row, to_column) and
+             is_allowed_colour(colour) do
+    from_column..to_column
+    |> Enum.reduce(canvas, fn column, canvas -> draw_in_cell(canvas, row, column, colour) end)
+  end
+
+  def draw_in_column(
+        %Canvas{row_count: row_count, column_count: column_count} = canvas,
+        column,
+        from_row,
+        to_row,
+        colour
+      )
+      when is_in_canvas(row_count, column_count, from_row, column) and
+             is_in_canvas(row_count, column_count, to_row, column) and is_allowed_colour(colour) do
+    from_row..to_row
+    |> Enum.reduce(canvas, fn row, canvas -> draw_in_cell(canvas, row, column, colour) end)
+  end
+
+  def flood(
+        %Canvas{row_count: row_count, column_count: column_count} = canvas,
+        row,
+        column,
+        new_colour
+      )
+      when is_in_canvas(row_count, column_count, row, column) and is_allowed_colour(new_colour) do
+    visited = MapSet.new()
+    original_colour = Map.get(canvas.cells, {row, column})
+    {new_canvas, _} = flood(canvas, row, column, original_colour, new_colour, visited)
+    new_canvas
+  end
+
+  defp flood(
+         %Canvas{cells: cells, row_count: row_count, column_count: column_count} = canvas,
+         row,
+         column,
+         original_colour,
+         new_colour,
+         visited
+       )
+       when is_in_canvas(row_count, column_count, row, column) and is_allowed_colour(new_colour) do
+    cond do
+      MapSet.member?(visited, {row, column}) ->
+        {canvas, visited}
+
+      Map.get(cells, {row, column}) !== original_colour ->
+        {canvas, visited}
+
+      true ->
+        new_visited = MapSet.put(visited, {row, column})
+        new_canvas = draw_in_cell(canvas, row, column, new_colour)
+
+        next_cells =
+          Enum.filter(
+            [
+              {row - 1, column},
+              {row + 1, column},
+              {row, column - 1},
+              {row, column + 1}
+            ],
+            &(!MapSet.member?(new_visited, &1))
+          )
+
+        Enum.reduce(next_cells, {new_canvas, new_visited}, fn {next_row, next_column},
+                                                              {new_canvas, new_visited} ->
+          flood(new_canvas, next_row, next_column, original_colour, new_colour, new_visited)
+        end)
+    end
+  end
+
+  defp flood(
+         %Canvas{} = canvas,
+         _row,
+         _column,
+         _original_colour,
+         new_colour,
+         visited
+       )
+       when is_allowed_colour(new_colour) do
+    {canvas, visited}
+  end
+
+  def draw(%Canvas{cells: cells, row_count: row_count, column_count: column_count}) do
+    1..row_count
+    |> Enum.map(&draw_columns(cells, column_count, &1))
+    |> Enum.join(IO.ANSI.black_background() <> "\n")
+    |> Kernel.<>(IO.ANSI.black_background())
+  end
+
+  defp draw_columns(cells, column_count, row) do
+    Enum.map(1..column_count, &draw_cell(cells, row, &1))
+    |> Enum.join()
+  end
+
+  defp draw_cell(cells, row, column) do
+    colour = Map.get(cells, {row, column}, @default_colour)
+    colour_fn = Map.get(@colour_to_ansi_fn, colour)
+    colour_fn.() <> " "
   end
 end
--- test/canvas_test.exs
+++ test/canvas_test.exs
@@ -2,7 +2,16 @@ defmodule CanvasTest do
   use ExUnit.Case
   doctest Canvas
 
-  test "greets the world" do
-    assert Canvas.hello() == :world
+  test "floods a canvas" do
+    c1 = Canvas.new(10, 10)
+    c2 = Canvas.draw_in_cell(c1, 4, 8, :blue)
+    c3 = Canvas.draw_in_row(c2, 1, 2, 9, :blue)
+    c4 = Canvas.draw_in_column(c3, 2, 1, 9, :blue)
+    c5 = Canvas.draw_in_row(c4, 3, 3, 7, :blue)
+    c6 = Canvas.draw_in_row(c5, 4, 3, 6, :green)
+    c7 = Canvas.flood(c6, 2, 2, :yellow)
+
+    assert Canvas.draw(c7) ==
+             "\e[41m \e[43m \e[43m \e[43m \e[43m \e[43m \e[43m \e[43m \e[43m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[43m \e[43m \e[43m \e[43m \e[43m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[42m \e[42m \e[42m \e[42m \e[41m \e[44m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[43m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m\n\e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[41m \e[40m"
   end
 end
